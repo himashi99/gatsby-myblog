aa21cddfeff1b1cf6012f8705b40320c598405ac
Himashi Hettege Dona <himashi99@hotmail.com>
2018-07-21 12:09:21 -0400

    Update blogpost font

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -16,7 +16,7 @@ export default ({ data }) => {
             paddingTop: 0,
           }}>
             <h1>{post.frontmatter.title}</h1>
-            <div dangerouslySetInnerHTML={{ __html: post.html }} />
+            <div className="blog-post-paragraph" dangerouslySetInnerHTML={{ __html: post.html }} />
           </div>
         </div>
           <FooterBlogPost/>
