c04534d523f2db939610a3c8dee6ba9fd1ac78b4
Himashi Hettege Dona <himashi99@hotmail.com>
2018-10-01 15:12:19 -0400

    Updated typo

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -4,7 +4,7 @@ import Link from 'gatsby-link'
 
 const SecondPage = () => (
   <div>
-    <h1>Hi, I{`'`} Himashi</h1>
+    <h1>Hi, I{`'`}m Himashi</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
     {/* <Rectangle/> */}
